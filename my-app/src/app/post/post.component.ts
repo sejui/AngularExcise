136e1212f97c465dc4cbb7bc7e572940ab48a6ba
Juey Yu <jyu2@central1.com>
2018-03-08 12:50:38 -0800

    [http-2] display the list of json file

--- my-app/src/app/post/post.component.ts
+++ my-app/src/app/post/post.component.ts
@@ -17,12 +17,14 @@ export class PostComponent implements OnInit {
   private body;
   private newPost;
   constructor(private _postService: PostService) {
-    this._postService.getPosts().subscribe(posts => {
-      this._posts = posts;
-    });
+
   }
 
   ngOnInit() {
+    // subscribe to the post service, so it will listen to the response and store in _posts.
+    this._postService.getPosts().subscribe(
+      response => this._posts = response.json()
+    );
   }
 
 }
